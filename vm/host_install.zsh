68cf9302da351f648b328030a8169f5f5f6a5a1f
Andrius Andrikonis <andrikonis.andrius@gmail.com>
2026-06-06 21:19:44 +0300

    Initializing VM on host

---
+++ vm/host_install.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+set -e
+
+readonly dir="${0:A:h}"
+
+virt-install \
+	--name "xiaomi-davinci-build" \
+	--os-variant "debian13" \
+	--virt-type "kvm" \
+	--memory "4096" \
+	--memorybacking "source.type=memfd,access.mode=shared" \
+	--vcpus "4" \
+	--cpu "host" \
+	--import \
+	--disk "path=$dir/assets/debian-13-nocloud-amd64.qcow2" \
+	--filesystem "type=mount,driver.type=virtiofs,source=$dir,target=source" \
+	--graphics "none" \
+	--boot "uefi"
